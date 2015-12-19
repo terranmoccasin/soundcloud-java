691a5ac13fa52ca33c49a07641b761d59f5dec1d
tmoc-mbp <terranmoccasin@gmail.com>
2015-12-19 23:02:38 +0800

    Added user resource.

--- src/main/java/fm/ensemble/soundcloud/resource/User.java
+++ src/main/java/fm/ensemble/soundcloud/resource/User.java
@@ -1,5 +1,144 @@
 package fm.ensemble.soundcloud.resource;
 
 public class User {
-
+  private int id;
+  private String permalink;
+  private String username;
+  private String uri;
+  private String permalinkUrl;
+  private String avatarUrl;
+  private String country;
+  private String fullName;
+  private String city;
+  private String description;
+  private String discogsName;
+  private String myspaceName;
+  private String website;
+  private String websiteTitle;
+  private boolean online;
+  private int trackCount;
+  private int playlistCount;
+  private int followersCount;
+  private int followingsCount;
+  private int publicFavoritesCount;
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getPermalink() {
+    return permalink;
+  }
+  public void setPermalink(String permalink) {
+    this.permalink = permalink;
+  }
+  public String getUsername() {
+    return username;
+  }
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  public String getUri() {
+    return uri;
+  }
+  public void setUri(String uri) {
+    this.uri = uri;
+  }
+  public String getPermalinkUrl() {
+    return permalinkUrl;
+  }
+  public void setPermalinkUrl(String permalinkUrl) {
+    this.permalinkUrl = permalinkUrl;
+  }
+  public String getAvatarUrl() {
+    return avatarUrl;
+  }
+  public void setAvatarUrl(String avatarUrl) {
+    this.avatarUrl = avatarUrl;
+  }
+  public String getCountry() {
+    return country;
+  }
+  public void setCountry(String country) {
+    this.country = country;
+  }
+  public String getFullName() {
+    return fullName;
+  }
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+  public String getCity() {
+    return city;
+  }
+  public void setCity(String city) {
+    this.city = city;
+  }
+  public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public String getDiscogsName() {
+    return discogsName;
+  }
+  public void setDiscogsName(String discogsName) {
+    this.discogsName = discogsName;
+  }
+  public String getMyspaceName() {
+    return myspaceName;
+  }
+  public void setMyspaceName(String myspaceName) {
+    this.myspaceName = myspaceName;
+  }
+  public String getWebsite() {
+    return website;
+  }
+  public void setWebsite(String website) {
+    this.website = website;
+  }
+  public String getWebsiteTitle() {
+    return websiteTitle;
+  }
+  public void setWebsiteTitle(String websiteTitle) {
+    this.websiteTitle = websiteTitle;
+  }
+  public boolean isOnline() {
+    return online;
+  }
+  public void setOnline(boolean online) {
+    this.online = online;
+  }
+  public int getTrackCount() {
+    return trackCount;
+  }
+  public void setTrackCount(int trackCount) {
+    this.trackCount = trackCount;
+  }
+  public int getPlaylistCount() {
+    return playlistCount;
+  }
+  public void setPlaylistCount(int playlistCount) {
+    this.playlistCount = playlistCount;
+  }
+  public int getFollowersCount() {
+    return followersCount;
+  }
+  public void setFollowersCount(int followersCount) {
+    this.followersCount = followersCount;
+  }
+  public int getFollowingsCount() {
+    return followingsCount;
+  }
+  public void setFollowingsCount(int followingsCount) {
+    this.followingsCount = followingsCount;
+  }
+  public int getPublicFavoritesCount() {
+    return publicFavoritesCount;
+  }
+  public void setPublicFavoritesCount(int publicFavoritesCount) {
+    this.publicFavoritesCount = publicFavoritesCount;
+  }
 }
